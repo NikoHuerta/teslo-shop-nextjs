8881eaee087f511f495d59e221d7b5dcb8c5c401
Nicolas Huerta <nhuertaf@icloud.com>
2022-05-31 12:31:31 -0400

    db bugfix

--- database/db.ts
+++ database/db.ts
@@ -13,7 +13,7 @@ const mongoConnection = {
 export const connect = async() => {
 
     if ( mongoConnection.isConnected ) {
-        // console.log('Ya estabamos conectados');
+        console.log('Ya estabamos conectados');
         return;
     }
 
@@ -21,7 +21,7 @@ export const connect = async() => {
         mongoConnection.isConnected = mongoose.connections[0].readyState;
 
         if ( mongoConnection.isConnected === 1 ) {
-            // console.log('Usando conexión anterior');
+            console.log('Usando conexión anterior');
             return;
         }
 
@@ -30,7 +30,7 @@ export const connect = async() => {
 
     await mongoose.connect( process.env.MONGODB_URI || '');
     mongoConnection.isConnected = 1;
-    // console.log('Conectado a MongoDB:', process.env.MONGODB_URI );
+    console.log('Conectado a MongoDB:', process.env.MONGODB_URI );
 }
 
 export const disconnect = async() => {
@@ -42,5 +42,5 @@ export const disconnect = async() => {
     await mongoose.disconnect();
     mongoConnection.isConnected = 0;
 
-    // console.log('Desconectado de MongoDB');
+    console.log('Desconectado de MongoDB');
 }
